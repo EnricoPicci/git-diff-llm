9fede4e3538ac705d07aad8a41ae598bef65f9f9
Enrico PIccinin <enrico.piccinin@gmail.com>
2024-11-07 09:34:20 +0100

    fix: update client address path to reflect new directory structure

--- src/lib/command.ts
+++ src/lib/command.ts
@@ -5,7 +5,7 @@ import { startWebServer } from '../core/start-web-server';
 
 startWebServer();
 
-const clientAddress = path.join(__dirname, '..', 'core', 'browser-client.html');
+const clientAddress = path.join(__dirname, '..', '..', 'src', 'core', 'browser-client.html');
 console.log(`clientAddress: file:/${clientAddress}\n`);
 console.log(`clientAddress (if using wsl and Ubuntu): file://wsl.localhost/Ubuntu${clientAddress}\n\n`);
 
