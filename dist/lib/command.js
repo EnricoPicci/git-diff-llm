379c767b8ba41e5f60a0c41f0bc190e0ee09ade4
Enrico PIccinin <enrico.piccinin@gmail.com>
2024-11-07 09:36:12 +0100

    1.0.5

--- dist/lib/command.js
+++ dist/lib/command.js
@@ -6,8 +6,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const start_web_server_1 = require("../core/start-web-server");
+// Read the version from package.json
+const packageJson = require('../../package.json');
+const version = packageJson.version;
+console.log(`Starting git-diff-llm server. Version: ${version}\n`);
 (0, start_web_server_1.startWebServer)();
-const clientAddress = path_1.default.join(__dirname, '..', 'core', 'browser-client.html');
+const clientAddress = path_1.default.join(__dirname, '..', '..', 'src', 'core', 'browser-client.html');
 console.log(`clientAddress: file:/${clientAddress}\n`);
 console.log(`clientAddress (if using wsl and Ubuntu): file://wsl.localhost/Ubuntu${clientAddress}\n\n`);
 // npm run tsc && node dist/lib/command.js
